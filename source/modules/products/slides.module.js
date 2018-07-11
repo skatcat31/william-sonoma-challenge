796dc7ddaa831cab0a98fb398040afcd0898214e
Robert Mennell <skatcat31@gmail.com>
2018-07-11 10:22:23 -0700

    crash on missing required fields

--- source/modules/products/slides.module.js
+++ source/modules/products/slides.module.js
@@ -3,6 +3,10 @@
  * @param {Object} product The product to generate the card for
  */
 function productSlides(product) {
+  // check fields we need
+  if (!product.name) throw new TypeError('The product needs a name');
+  if (!product.hero || !product.hero.href) throw new TypeError('The product needs a hero with a link to the image');
+
   // the container
   const preview = document.createElement('div');
   preview.classList.add('img__container');
@@ -19,8 +23,8 @@ function productSlides(product) {
   hover.appendChild(text);
 
   // the price
-    const price = document.createElement('div');
-    price.classList.add('img__text-no-top-pad');
+  const price = document.createElement('div');
+  price.classList.add('img__text-no-top-pad');
   if (product.priceRange && product.priceRange.selling) {
     /* eslint-disable no-nested-ternary */
     price.innerText = product.priceRange.selling.low && product.priceRange.selling.high
