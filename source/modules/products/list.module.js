45af430999965279156b6a6882d7d9db38c586b9
Robert Mennell <skatcat31@gmail.com>
2018-07-11 10:29:26 -0700

    If there are no images, don't add the click handler

--- source/modules/products/list.module.js
+++ source/modules/products/list.module.js
@@ -11,15 +11,15 @@ import previewStyle from './slides.style.js';
 function productsList(products, slideShow) {
   // crash on missing setState function
   if (typeof slideShow.setState !== 'function') throw new TypeError('The slideShow must expose a setState function');
-
+  
   // create a parent
   const parent = document.createElement('div');
-
+  
   // register the child
   parent.appendChild(style);
   parent.appendChild(previewStyle);
   parent.classList.add('responsive-children');
-
+  
   // Container
   const productInfo = document.createElement('div');
   productInfo.classList.add('wide');
@@ -28,7 +28,9 @@ function productsList(products, slideShow) {
   // Generate and register the cards
   products.forEach((product) => {
     const item = productInfo.cloneNode(true);
-    item.onclick = () => slideShow.setState({ images: product.images });
+    if (product.images && product.images.length) {
+      item.onclick = () => slideShow.setState({ images: product.images });
+    }
     item.appendChild(productSlide(product));
     return parent.appendChild(item);
   });
